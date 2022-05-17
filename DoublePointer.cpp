5981bc6c475448a5369ae222d0bbb0509e28c95d
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2022-05-17 11:42:15 +0530

    DoublePointer_CPP

--- DoublePointer.cpp
+++ DoublePointer.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void update(int **p2)
+{
+    // p2 = p2+ 1;         // No change because it's came through pass by value method and the changes is done only in the function
+
+    // *p2 = *p2 + 1;         // Yes value of single pointer will change in the main after calling the function
+
+    **p2 = **p2 + 1;        // Yes the vsalue of i will be change in the main method after function is called
+}
+
 int main()
 {
     // DOUBLE POINTERS
@@ -8,7 +17,7 @@ int main()
     int i = 5;
     int *p = &i;
     int **p2 = &p;
-
+    /*
     // PRINT 5 value at i
     cout << " VALUE " << endl;
     cout << i << endl;
@@ -38,5 +47,15 @@ int main()
     // VALUE AT p2
     cout << " VALUE AT p2 " << endl;
     cout << p2 << endl;
+    */
+
+    cout << "Before i  =" << i << endl;
+    cout << "Before p  =" << p << endl;
+    cout << "Before p2  =" << p2 << endl;
+    update(p2);
+    cout << "After i  =" << i << endl;
+    cout << "After p  =" << p << endl;
+    cout << "After p2  =" << p2 << endl;
+
     return 0;
 }
