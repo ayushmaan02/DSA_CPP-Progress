a7b86e5cf7f10404b6443bc7f3a0a29200ea5c83
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2022-05-17 15:29:14 +0530

    PointerQues_CPP

--- PointerQuestions.cpp
+++ PointerQuestions.cpp
@@ -18,11 +18,53 @@ int main()
     // cout << f << endl;
     // // Output =  7
 
-    // Q3
-    int f = 8;
-    int *p = &f;
-    cout<< (*p)++ << " ";
-    cout << f << endl;
-    // Output =  8 9
+    // // Q3
+    // int f = 8;
+    // int *p = &f;
+    // cout<< (*p)++ << " ";
+    // cout << f << endl;
+    // // Output =  8 9
+
+    // // Q4
+    // int *p = 0;
+    // int f = 8;
+    // *p = f;
+    // cout << *p << endl;
+    // // Output =  Segmentation fault
+
+    // // Q5
+    // int f = 8, s = 11;
+    // int *third = &s;
+    // f = *third;
+    // *third = *third + 2;
+    // cout << f <<" " <<s << endl;
+    // // Output = 11 13
+
+    // // Q6
+    // float f = 12.5, p = 21.5;
+    // float *ptr = &f;
+    // (*ptr)++;
+    // *ptr = p;
+    // cout << *ptr << " " << f << " " << p << endl;
+    // // Output = 11 13
+
+    // // Q7
+    // int arr[7] = {11, 21, 31};
+    // int *p = arr;
+    // cout << p[2] << endl;
+    // // Output = 31
+
+    // // Q8
+    // char ch[] = "abcde";
+    // char *c = &ch[0];
+    // cout << c << endl;
+    // // Output = abcde
+
+    // Q9
+    char ch[] = "abcde";
+    char *c = &ch[0];
+    c++;
+    cout << c << endl;
+    // Output = bcde
     return 0;
 }
