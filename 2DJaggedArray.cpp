7f1c164d16099319da0211f179e442aba47adb9f
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2022-06-22 12:02:45 +0530

    2DJaggedArray

---
+++ 2DJaggedArray.cpp
@@ -0,0 +1,15 @@
+// Dynamic 2D jagged array
+// 1. Declare an array of pointers (jagged array),
+// 2. The size of this array will be the number of rows required in the Jagged array
+// 3. Then for each pointer in the array allocate memory for the number of elements you want in this row.
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+
+int main()
+{
+    
+    return 0;
+}
