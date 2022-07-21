395ef6e9aac164d1ec8781d6d2daaa3574640615
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2022-07-21 15:03:02 +0530

    OOPs Method Overriding

---
+++ OOPs/Method_FunctionOverriding.cpp
@@ -0,0 +1,37 @@
+/*Rules
+1. The name of the method in parent and child class should be same
+2. The parameters should also be same
+3. It is possible through inheritance only
+*/
+
+
+#include<iostream>
+using namespace std;
+class Human
+{
+    public:
+        void speak()
+        {
+            cout<<"Speaking"<<endl; 
+        }
+};
+
+class Dog: public Human
+{
+    public:
+        void speak()
+        {
+            cout<<"Barking"<<endl;
+        }
+};
+
+
+
+int main()
+{
+    Dog d1;
+    Human h1;
+    d1.speak();
+    h1.speak();
+    return 0;
+}
