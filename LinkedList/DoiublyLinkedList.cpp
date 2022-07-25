4d3ada691ad371049b7c9c7d26f104fec44fb8b3
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2022-07-25 19:56:57 +0530

    DLL NULL handled

--- LinkedList/DoiublyLinkedList.cpp
+++ LinkedList/DoiublyLinkedList.cpp
@@ -41,24 +41,93 @@ int getLength(Node *head)
     return len;
 }
 
-void InsertAtHead(Node *&head, int d)
+void InsertAtHead(Node *&head, Node *&tail, int d)
 {
-    Node* temp = new Node(d);
-    temp -> next = head;
-    head -> prev = temp;
-    head = temp;
+    if (head == NULL)       //When the linked list is empty 
+    {
+        /* code */
+        Node *temp = new Node(d);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node *temp = new Node(d);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+}
+
+void insertAtTail(Node *&head, Node *&tail, int data)
+{
+    if (tail == NULL)               //When the linked list is empty 
+    {
+        Node *temp = new Node(data);
+        tail = temp;
+        head = temp;
+    }
+
+    else
+    {
+        Node *temp = new Node(data);
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }
+}
+
+void InsertAtPosition(Node *&head, Node *&tail, int pos, int data)
+{
+    if (pos == 1)
+    {
+        InsertAtHead(head,tail, data);
+        return;
+    }
+
+    Node *temp = head;
+    int cnt = 1;
+    while (cnt < pos - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    if (temp->next == NULL)
+    {
+        insertAtTail(head, tail, data);
+        return;
+    }
+
+    Node *nodeToAdd = new Node(data);
+    nodeToAdd->next = temp->next;
+    temp->next->prev = nodeToAdd;
+    temp->next = nodeToAdd;
+    nodeToAdd->prev = temp;
 }
 
 int main()
 {
-    Node *first = new Node(10);
-    Node *head = first;
+    Node *head = NULL;
+    Node *tail = NULL;
     print(head);
     cout << "Length of the DLL is " << getLength(head) << endl;
 
-    InsertAtHead(head, 11);
+    InsertAtHead(head,tail, 200);
     print(head);
-    cout << "Length of the DLL is " << getLength(head) << endl;
+
+    insertAtTail(head, tail, 300);
+    print(head);
+
+
+    InsertAtPosition(head, tail, 1, 100);
+    print(head);
+
+    InsertAtPosition(head, tail, 4, 400);
+    print(head);
+
+    InsertAtPosition(head, tail, 5, 500);
+    print(head);
 
     return 0;
 }
