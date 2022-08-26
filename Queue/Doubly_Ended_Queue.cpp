1528e4ff0b8be4564498f226c20fe8da097c4f49
Ayushmaan Singh Rajput <66905426+ayushmaan02@users.noreply.github.com>
2022-08-26 18:33:51 +0530

    Deque using STL

---
+++ Queue/Doubly_Ended_Queue.cpp
@@ -0,0 +1,23 @@
+//Doubley Ended Queue
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main()
+{
+    deque<int> d;
+
+    d.push_front(12);
+    d.push_back(14);
+
+    cout<<d.front()<<endl;
+    cout<<d.back()<<endl;
+
+    d.pop_front();
+
+    cout<<d.front()<<endl;
+    cout<<d.back()<<endl;
+
+    return 0;
+}
